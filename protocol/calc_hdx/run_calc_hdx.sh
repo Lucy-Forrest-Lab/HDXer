7e6d5e1ea9b34fa806fd74a115886679136a1a90
Suhwan Paul Lee <suhwanplee@gmail.com>
2021-03-12 08:28:11 -0700

    Delete protocol/calc_hdx directory

--- protocol/calc_hdx/run_calc_hdx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# A Bash script to run calc_hdx.py
-
-python $HDXER_PATH/HDXer/calc_hdx.py -t $BPTI_PATH/structural_ensembles/BPTI_full.dcd -p $BPTI_PATH/structural_ensembles/BPTI_full.pdb -m Radou -log BPTI.log -out BPTI_ -exp $BPTI_PATH/expt_data/BPTI_expt_dfracs.dat -seg $BPTI_PATH/expt_data/BPTI_residue_segs.txt -mopt "{ 'save_detailed' : True }"
